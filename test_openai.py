746b0d2dd5fefd6e50bc704a5c3e96af5de40403
shnoorabad <amasomi_376@yahoo.com>
2025-05-18 12:29:39 +0330

    Update test_openai.py

--- test_openai.py
+++ test_openai.py
@@ -1,11 +1,1 @@
-import os
-from openai import OpenAI
 
-client = OpenAI(api_key=os.environ["OPENAI_KEY"])
-
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "سلام"}]
-)
-
-print(response.choices[0].message.content)
